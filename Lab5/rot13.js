bb6a1873fb291a9174d29bf1edd5105c803da701
Laughlan McGregor <40311738@live.napier.ac.uk>
2019-02-12 12:28:49 +0000

    lab5 code

--- Lab5/rot13.js
+++ Lab5/rot13.js
@@ -2,11 +2,11 @@ function encode()
 {
 	var plain_text = document.getElementById("message").value;
 	var cypher_text = [];
-	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 	
 	for(var idx=0; idx<plain_text.length; idx++)
 	{
-		input = alphabet.index0f(plain_text[idx]);
+		input = alphabet.indexOf(plain_text[idx]);
 		if(input == -1 )
 		{
 			cypher_text.push(plain_text[idx]);
@@ -15,7 +15,7 @@ function encode()
 		{
 			var coded = (input+13)%26;
 			var letter = alphabet[coded];
-			cypher_text.push(letters);
+			cypher_text.push(letter);
 		}
 	}
 	document.getElementById("output").innerHTML = cypher_text.join("");
